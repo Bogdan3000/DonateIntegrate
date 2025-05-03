7e21d117835b64aa03f48be5a6405b94b6f36ef9
Bohdan <ihnatenko.bogdan@gmail.com>
2025-05-04 00:30:10 +0200

    Initial commit

---
+++ src/main/java/com/bogdan3000/dintegrate/DonateIntegrate.java
@@ -0,0 +1,19 @@
+package com.bogdan3000.dintegrate;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = "dintegrate", name = "DonateIntegrate", version = "1.0.0")
+public class DonateIntegrate {
+
+    @EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public static void init(FMLInitializationEvent event) {
+    }
+}
